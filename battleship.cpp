9f5568efd9af7142dbb2cbf3a9e826d651fc3513
Hudson <huddy987@gmail.com>
2018-11-10 22:39:14 -0700

    Functionized stuff

--- battleship.cpp
+++ battleship.cpp
@@ -2,6 +2,8 @@
 #include <SPI.h>             // Library for SPI mode
 #include <Adafruit_ILI9341.h> // Controller chip library
 #include "TouchScreen.h"    //Library for TouchScreen
+#include "touch_handler.h"  // touch handler header file
+#include "draw_handler.h" // draw handler header file
 
 
 // These are the four touchscreen analog pins
@@ -32,22 +34,6 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 // Define grid box size (40x40)
 #define BOXSIZE 40
 
-void draw_empty_grid(){
-  tft.fillScreen(ILI9341_BLACK);
-
-  // Draws a 6 x 7 grid of 40x40 pixel boxes
-  // Starts at index 0 because 40*0=0
-  for (int i = 1; i < 8; i++){
-    for (int j = 1; j < 7; j++){
-      tft.drawRect((tft.width() - BOXSIZE * j), (tft.height() - BOXSIZE * i), BOXSIZE, BOXSIZE, ILI9341_WHITE);
-    }
-  }
-  // Draw a placeholder for the menu
-  // TODO: Replace this with something meaningful/useful
-  tft.fillRect(0, 0, 120, BOXSIZE, ILI9341_RED);
-  tft.fillRect(120, 0, 120, BOXSIZE, ILI9341_GREEN);
-}
-
 void setup() {
   init();
   Serial.begin(9600);
@@ -55,11 +41,16 @@ void setup() {
 
   tft.begin();
   // Fill the screen with black (for background)
-  draw_empty_grid();
+  draw_empty_grid(tft, BOXSIZE);
+  // Placeholder menu TODO replace
+  tft.fillRect(0, 0, 120, BOXSIZE, ILI9341_RED);
+  tft.fillRect(120, 0, 120, BOXSIZE, ILI9341_GREEN);
 }
 
 int main(){
   setup();
+  // Check this out! Draws a pink rectangle at given coordinates TODO: remove this
+  draw_at_grid_pos(tft, BOXSIZE, "D3", ILI9341_PINK);
   while (1){
     loop();
   }
@@ -81,19 +72,23 @@ void loop(){
 
   // Scale from 0->1000 to tft.width and tft.height using the calibration #'s
 
-  //WARNING: Oddly enough, there is a deadzone at the top of the display if we don't add the +10. - Hudson
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width() + 10);
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
 
-  // TODO: Round these values to the center of each grid box and check if touch is inside a menu button.
+  /*
   Serial.println(p.x);
   Serial.println(p.y);
+  */
 
   // If we are inside the menu region...
   if (p.y < 40){
     // If we are in the top half of the menu...
     if (p.x > 120){
-      draw_empty_grid();
+      // Draw empty grid
+      draw_empty_grid(tft, BOXSIZE);
+      // Placeholder menu TODO replace
+      tft.fillRect(0, 0, 120, BOXSIZE, ILI9341_RED);
+      tft.fillRect(120, 0, 120, BOXSIZE, ILI9341_GREEN);
     }
     // If we are in the bottom half of the menu...
     else if (p.x < 120) {
@@ -103,25 +98,15 @@ void loop(){
     }
   }
 
-  // If we are inside the grid reion...
+  // If we are inside the grid region get the grid position
   else if (p.y > 40){
-    // Start at the largest box size
-    for (int i = 8; i > -1; i--){
-      // If the y coordinate is greater then the multiple of the box, we know it is inside the box at that index.
-      if (p.y >= (BOXSIZE * i)){
-        // Start at the largest box size
-        for (int j = 7; j > -1; j--){
-          // If the x coordinate is greater then the multiple of the box, we know it is inside the box at that index.
-          if (p.x >= (BOXSIZE * j)){
-            // Draw a red box inside the correct box
-            tft.fillRect((BOXSIZE * j), (BOXSIZE * i), BOXSIZE, BOXSIZE, ILI9341_RED);
-            // Break here so we don't continually draw boxes in the x direction
-            break;
-          }
-        }
-        // Break here so we don't continually draw boxes in the y direction
-        break;
-      }
-    }
-  }
+    String grid_pos = get_grid_position(p, BOXSIZE);
+    draw_at_grid_pos(tft, BOXSIZE, grid_pos, ILI9341_NAVY);
+    Serial.print(grid_pos);
+    delay(500);
+    /*
+      Since this is a string, it will be very easy for decomposition:
+      grid_pos[0] returns the A-G value, grid_pos[1] returns the 0-5 value
+    */
+}
 }
---
+++ touch_handler.h
@@ -0,0 +1,9 @@
+#ifndef _TOUCH_HANDLER_H_
+#define _TOUCH_HANDLER_H_
+
+// header file for touch handler
+
+String get_grid_position(TSPoint point, int BOXSIZE);
+
+
+#endif
