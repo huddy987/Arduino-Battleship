e8ae16371a1f4f776fc44133332d5f6fd3b442cb
cmput274 <cmput274@ualberta.ca>
2018-11-12 01:26:19 -0700

    some more stuff

--- data_handler.cpp
+++ data_handler.cpp
@@ -33,19 +33,146 @@
 */
 
 uint16_t determine_array_element(String grid_pos){
-  //idk if I can index strings in C++ so this should be fun
+  //idk if I can index strings in C++ so this should be fun to test
   uint16_t column = static_cast<int>(grid_pos[0]) - 64;
   uint16_t row = static_cast<int>(grid_pos[1]) - 48;
   uint16_t element = column + (row * 7) -1;
   return element
   }
 
-// the input of this function will be the array, the block number
+/* if all parts of the boat with boat_id has been shot,
+  this function will return a 1. otherwise, a 0.
+*/
+uint16_t check_boat(uint16_t player_array[][2], uint16_t boat_id){
+  for(int i=0;i<41;i++) {
+    if (**(player_array + i) == boat_id){
+      uint16_t block_state = *(*(player_array + i) + 1);
+      if ((block_state = 2 or  ))
+    }
+  }
+}
+
+
+
+
+// the input of this function will be the array and the block number
+// I imagine this being called when the player makes a turn.
+void recieve_turn(uint16_t player_array[][2], uint16_t block_number){
+  /*
+  *(*(player_array + block_number)) == the boat ID
+  *(*(player_array + block_number) + 1) == the block state */
+
+  switch (*(*(player_array + block_number) + 1)) {
+    case 0 :  // undisturbed
+      *(*(player_array + block_number) + 1) = 1;
+      // Serial3.write a 5
+      break;
+    case 2 :  // boat hidden; not shot
+      *(*(player_array + block_number) + 1) = 3;
+      // TODO: check if all parts of this boat has been shot;
+              // if yes this should be  = 4; Serial3.write a 7
+      // else Serial3.write a 6
+      break;
+    default :
+      Serial.print("Error");
+  }
+
+void after_enemy_update(uint16_t player_array[][2], uint16_t block_number){
+  // Serial3.read what they sent.
+  // uint16_t enemy_response = Serial3.read()
+  switch (*(*(player_array + block_number) + 1)) {
+    case 5:  // undisturbed
+      *(*(player_array + block_number) + 1) = 5;
+      // make the block a 5 on the enemy's side by serial3.write
+      break;
+    case 6:  // boat hidden; not shot
+      *(*(player_array + block_number) + 1) = 6;
+      // check if all parts of this boat has been shot; if yes = 4
+      /// make the block a 6 on the enemy's side
+      break;
+    case 7:
+    // look for the other parts of this boat and make them 7
+    // do this by iterating over 2d array: if same boat id: make 7
+    /*
+    for (i=0;i<41;i++) {
+    if **(player_array + i) == boat id of
+  }
+    */
+    break;
+    default : //Optional
+      Serial.print("Error");
+}
+
+/*
+void update_game_array(uint16_t player_array[][2], uint16_t block_number){
+    /*
+    *(*(player_array + block_number)) == the boat ID
+    *(*(player_array + block_number) + 1) == the block state
+    // *(*(arramina + 3)+1); 4th row, 2nd column
+
+    switch(*(*(player_array + block_number) + 1)) {
+      case 0 :  // undisturbed
+        *(*(player_array + block_number) + 1;
+        break;
+      case 1 :  // shot but no boat
+        statement(s);
+        break;
+      case 2 :  // boat hidden; not shot
+        statement(s);
+        break;
+      case 3 :  // shot boat
+        statement(s);
+        break;
+      case 4 :  // full boat sunk -- for graphics
+        statement(s);
+        break;
+      case 5 :  // enemy's: shot but no boat
+        statement(s);
+        break;
+      case 6 :  // enemy's: shot and yes boat
+        statement(s);
+        break;
+      case 7 :  // enemy's: full boat sunk
+        statement(s);
+        break;
+     default : //Optional
+        Serial.print("Error");
+  }
+} */
+
+
 void update_game_array(uint16_t player_array[][2], uint16_t block_number){
   /*
   *(*(player_array + block_number)) == the boat ID
   *(*(player_array + block_number) + 1) == the block state */
   // *(*(arramina + 3)+1); 4th row, 2nd column
-  if *(*(player_array + block_number))
 
+  switch(*(*(player_array + block_number) + 1)) {
+    case 0 :  // undisturbed
+      *(*(player_array + block_number) + 1;
+      break;
+    case 1 :  // shot but no boat
+      statement(s);
+      break;
+    case 2 :  // boat hidden; not shot
+      statement(s);
+      break;
+    case 3 :  // shot boat
+      statement(s);
+      break;
+    case 4 :  // full boat sunk -- for graphics
+      statement(s);
+      break;
+    case 5 :  // enemy's: shot but no boat
+      statement(s);
+      break;
+    case 6 :  // enemy's: shot and yes boat
+      statement(s);
+      break;
+    case 7 :  // enemy's: full boat sunk
+      statement(s);
+      break;
+   default : //Optional
+      Serial.print("Error");
+}
 }
